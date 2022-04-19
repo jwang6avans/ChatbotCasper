adbfd0774d27b24c0edeb98b689454c5400d1185
Jenny <jenny.jia.wang@hotmail.com>
2022-04-19 22:39:42 +0200

    change

--- src/Chatbot/index.jsx
+++ src/Chatbot/index.jsx
@@ -17,6 +17,13 @@ import { ThemeProvider } from "styled-components";
 //git push
 //npm run deploy
 
+//git show-ref
+//git fetch   --update all new info from github
+// git checkout gh-pages // go to the gh-pages branch
+// git rebase master // bring gh-pages up to date with master
+// git push origin gh-pages // commit the changes
+// git checkout master // return to the master branch
+
 
 const BotRedirect = ({ url, message }) => {
   return (
