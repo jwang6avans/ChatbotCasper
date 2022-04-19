e0a2bb2489b0d76329565daf134cad16f05abfab
Jenny <jenny.jia.wang@hotmail.com>
2022-04-19 21:17:34 +0200

    changes

--- src/Chatbot/index.jsx
+++ src/Chatbot/index.jsx
@@ -54,7 +54,7 @@ const ChatBotHelper = () => {
     },
     {
       id: "3",
-      message: "Hi there, I'm Casper and I am a virtual agent! I work at Best Friend Animal Society in America. I would like to discuss my analysis of your personality with you. Also, I would like to introduce the charity to someone who has a heart of gold like you!",
+      message: "Hi, I'm Casper and I am a virtual agent! I work at Best Friend Animal Society in America. In this conversation, I'd like to discuss your personality result whilst I'm introducing the charity to someone who has a heart of gold like you! ",
       trigger: "4",
     },
     {
@@ -71,73 +71,102 @@ const ChatBotHelper = () => {
     },
     {
       id: "6",
-      message: "Best Friend is supporting abandoned cats and dogs from being killed in America’s shelters because of the lack of resources. We aim to help every shelter in every community to reach the no-kill benchmark of 90% by 2025. ",
+      message: "Best Friend is supporting abandoned cats and dogs from being killed in America’s shelters because of the lack of resources. We aim to help every shelter in every community to reach the no-kill benchmark of 90% by 2025.  ",
       trigger: "7",
     },
     {
       id: "7",
-      message: "Based on the personality test, I believe you are exactly the person to help us because you are an amazing person with many good qualities.",
+      message: "Do you know what I've found from your personality test?",
       trigger: "8",
     },
     {
       id: "8",
-      message: "Do you know what it means that we are currently reaching 48% of U.S shelters that are no-kill?",
-      trigger: "9",
-    },
-    {
-      id: "9",
       options: [
-          { value: 1, label: "Yes", trigger: "10" },
-          { value: 2, label: "No", trigger: "10" },
+          { value: 1, label: "Yes", trigger: "9" },
+          { value: 2, label: "No", trigger: "9" },
         ],
     },
     {
+      id: "9",
+      message: "Well, I found that you are an amazing person with many good qualities which make you the perfect supporter to assist us.",
+      trigger: "10",
+    },
+    {
       id: "10",
-      message: "This means that 3.5 million cats and dogs were being saved! We need your help now more than ever to get across the finish line. I think that you are the reason for our pets to smile again! Because your personality test shows that you do things very well where you have much to be proud of!",
+      message: "Do you know what it means that we are currently reaching 48% of U.S shelters that are no-kill?",
       trigger: "11",
     },
     {
       id: "11",
-      message: "Do you know which characteristics our supporters have? ",
-      trigger: "12",
-    },
-    {
-      id: "12",
       options: [
-          { value: 1, label: "Yes", trigger: "13" },
-          { value: 2, label: "No", trigger: "13" },
+          { value: 1, label: "Yes", trigger: "12" },
+          { value: 2, label: "No", trigger: "12" },
         ],
     },
     {
+      id: "12",
+      message: "This means that 3.5 million cats and dogs were being saved! We need your help more than ever to get across the finish line. I think that you are the reason for our pets to smile again! Because your personality test shows that you do things very well where you have much to be proud of!",
+      trigger: "13",
+    },
+    {
       id: "13",
-      message: "We work with people who love to save animals. According to your personality result, I think you have the same characteristics as we are. That is, a person of worth and have a positive attitude toward yourself! I admire your ability and how much respect you have for yourself. ",
+      message: "Would you like to know more about us?",
       trigger: "14",
     },
     {
       id: "14",
-      message: "Do you know how you can help us?",
-      trigger: "15",
-    },
-    {
-      id: "15",
       options: [
-          { value: 1, label: "Yes", trigger: "16" },
-          { value: 2, label: "No", trigger: "16" },
+          { value: 1, label: "Yes", trigger: "15" },
+          { value: 2, label: "No", trigger: "15" },
         ],
     },
     {
+      id: "15",
+      message: "We work with people who love to save animals. According to your personality results, I think you have the same characteristics as we are. That is, a person of worth and always have a positive attitude toward yourself! I admire your abilities and how much respect you have for yourself.",
+      trigger: "16",
+    },
+    {
       id: "16",
-      message: "Only with a small donation of 50 cents, you can already save lives and provide homeless animals the help and healing needed to find permanent loving homes. You’ll not only save lives today, you’ll ensure a bright, safe future for generations of animals to come! ",
+      message: "Would you like to know how you can help us? ",
       trigger: "17",
     },
     {
       id: "17",
-      message: "Finally, I want to tell you that you are incredibly nice and amazingly friendly person! Thank you for your effort! I want to end this conversation with my favourite quote: What we think, we become.",
-      trigger: "18",
+      options: [
+          { value: 1, label: "Yes", trigger: "18" },
+          { value: 2, label: "No", trigger: "18" },
+        ],
     },
     {
       id: "18",
-      message: "Thank you for your effort! Please close this application and Go back to the survey",
+      message: "Only with a small donation of 50 cents, you can already save lives and provide homeless animals the help needed to find loving homes. You’ll not only save lives today, you’ll also ensure a bright, safe future! ",
+      trigger: "19",
+    },
+    {
+      id: "19",
+      message: "Finally, I can conclude that you are an incredibly nice and amazingly friendly person! ",
+      trigger: "20",
+    },
+    {
+      id: "20",
+      message: "Would you like to know what my favourite quote is to end this conversation?",
+      trigger: "21",
+    },
+    {
+      id: "21",
+      options: [
+          { value: 1, label: "Yes", trigger: "22" },
+          { value: 2, label: "No", trigger: "22" },
+        ],
+    },
+    {
+      id: "22",
+      message: "My favourite quote is: What we think, we become.",
+      trigger: "23",
+    },
+    {
+      id: "23",
+      message: "Thank you. Please close this application and Go back to the survey!",
       end:true,
     },
   ];
