daeeddee71d972dee0d9427c80f446a4d5b4bb0d
Jenny <jenny.jia.wang@hotmail.com>
2022-04-19 17:41:06 +0200

    deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import Chatbot from "./Chatbot";
+import "./Animal Charity Best Friend.png";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chatbot />
+      <img src={require("./Animal Charity Best Friend.png")} height ={700} width = {1200}/>
     </div>
   );
 }
 
-export default App;
+
+
