1be2e76664470a95e4f223cfc3ce5213efa8aaee
Jenny <jenny.jia.wang@hotmail.com>
2022-04-26 19:25:20 +0200

    version PT010

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import "./styles.css";
 import Chatbot from "./Chatbot";
 import "./Animal Charity Best Friend.png";
+//import PreLoader1 from "./components/PreLoader1";
 
 
 export default function App() {
   return (
-    <div className="App">
-      <Chatbot />
-      <img src={require("./Animal Charity Best Friend.png")} height ={700} width = {1200}/>
+    <div>
+        <div className="App">
+            <Chatbot />
+            <img src={require("./Animal Charity Best Friend.png")} height ={700} width = {1200}/>
+        </div>
     </div>
   );
 }
 
-
-
+//https://www.youtube.com/watch?v=SR8755C0bME
+//https://www.npmjs.com/package/react-spinners
+          //<Chatbot />
+          //<img src={require("./Animal Charity Best Friend.png")} height ={700} width = {1200}/>
