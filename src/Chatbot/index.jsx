2781cb6ec4f40d1f117debb79cf0006672bd0721
Jenny <jenny.jia.wang@hotmail.com>
2022-04-28 14:43:54 +0200

    adjust PT010

--- src/Chatbot/index.jsx
+++ src/Chatbot/index.jsx
@@ -61,52 +61,51 @@ const ChatBotHelper = () => {
     },
     {
       id: "3",
-      delay: 2000,
-      message: "Hi, I'm Casper and I am a virtual agent! I work at Best Friend Animal Society in America. In this conversation, I'd like to discuss your personality result whilst I'm introducing the charity to you. ",
+      delay: 4000,
+      message: "Hi, I'm Casper and I am a virtual agent! I work at Best Friend Animal Society in America.",
       trigger: "4",
     },
-    {
+     {
       id: "4",
       delay: 5000,
-      message: "Have you ever heard about Best Friend?",
+      message: "In this conversation, I'd like to discuss your personality result whilst I'm introducing the charity to you.",
       trigger: "5",
     },
     {
       id: "5",
-      options: [
-          { value: 1, label: "Yes", trigger: "6" },
-          { value: 2, label: "No", trigger: "6" },
-        ],
+      delay: 5000,
+      message: "Have you ever heard about Best Friend?",
+      trigger: "6",
     },
     {
       id: "6",
-      delay: 4000,
-      message: "Best Friend is supporting abandoned cats and dogs from being killed in America’s shelters because of the lack of resources. The charity started to create sanctuary for animals who are homeless and need special help. Today, we came closer to our goal together with our network partners and pet lifesaving centers. ",
-      trigger: "7",
+      options: [
+          { value: 1, label: "Yes", trigger: "7" },
+          { value: 2, label: "No", trigger: "7" },
+        ],
     },
     {
       id: "7",
-      delay: 9000,
-      message: "Are you interested to know what we aim for?",
+      delay: 5000,
+      message: "Best Friend is supporting abandoned cats and dogs from being killed in America’s shelters because of the lack of resources.",
       trigger: "8",
     },
     {
       id: "8",
-      options: [
-          { value: 1, label: "Yes", trigger: "9" },
-          { value: 2, label: "No", trigger: "9" },
-        ],
+      delay: 5000,
+      message: "The charity started to create sanctuary for animals who are homeless and need special help.",
+      trigger: "9",
     },
     {
       id: "9",
-      delay: 4000,
-      message: "Each day, 951 animals on average are being killed in America shelters. Since 2016, we declare to change this by given help to every shelter in every community to reach the no-kill benchmark of 90% by 2025. We have made incredible process in the last 5 years. ",
+      delay: 5000,
+      message: "Today, we came closer to our goal together with our network partners and pet lifesaving centers.",
       trigger: "10",
     },
     {
       id: "10",
-      delay: 9000,
-      message: "Do you want to know what I've found from your personality test?",
+      delay: 5000,
+      message: "Are you interested to know what we aim for?",
       trigger: "11",
     },
     {
@@ -118,92 +117,166 @@ const ChatBotHelper = () => {
     },
     {
       id: "12",
-      delay: 4000,
-      message: "Well, your personality scores shows that you have very much to be proud of, and that you are very competent. The majority of our supporters have similar result. We love to work with people love and kindness to all living being.",
+      delay: 5000,
+      message: "Each day, 951 animals on average are being killed in America shelters.",
       trigger: "13",
     },
     {
       id: "13",
-      delay: 9000,
-      message: "Are you interested to know how much progress we made this year?",
+      delay: 5000,
+      message: "Since 2016, we declare to change this by given help to every shelter in every community to reach the no-kill benchmark of 90% by 2025.",
       trigger: "14",
     },
     {
       id: "14",
-      options: [
-          { value: 1, label: "Yes", trigger: "15" },
-          { value: 2, label: "No", trigger: "15" },
-        ],
+      delay: 5000,
+      message: "We have made incredible process in the last 5 years. ",
+      trigger: "15",
     },
     {
       id: "15",
-      delay: 4000,
-      message: "This year, 48% of the 4404 shelters in U.S. have achieved a save rate of 90% or more. This means that 3.5 million cats and dogs have been saved! 85% of our supporters show with their personality test results that they are an great person with many good qualities just like you. Therefore, we need your help more than ever to get across the finish line.",
+      delay: 9000,
+      message: "Do you want to know what I've found from your personality test?",
       trigger: "16",
     },
     {
       id: "16",
-      delay: 9000,
-      message: "Interested to find out more about us?",
-      trigger: "17",
-    },
-    {
-      id: "17",
       options: [
-          { value: 1, label: "Yes", trigger: "18" },
-          { value: 2, label: "No", trigger: "18" },
+          { value: 1, label: "Yes", trigger: "17" },
+          { value: 2, label: "No", trigger: "17" },
         ],
     },
     {
+      id: "17",
+      delay: 5000,
+      message: "Well, your personality scores shows that you have very much to be proud of, and that you are very competent.",
+      trigger: "18",
+    },
+    {
       id: "18",
-      delay: 4000,
-      message: "We work with people who love to save animals. We respect one another and operate with love and seek to spread positivity. I believe we share a lot of similar traits. Your personality test results reveals that you are a person of worth and someone who has really much respect for yourself!",
+      delay: 5000,
+      message: "You share a lot of similar traits with many of our supporters.",
       trigger: "19",
     },
     {
       id: "19",
-      delay: 9000,
-      message: "Are you interested to find out how you can help us?",
+      delay: 5000,
+      message: "We love and respect to work with people with love and seek to spread positivity to all living being.",
       trigger: "20",
     },
     {
       id: "20",
-      options: [
-          { value: 1, label: "Yes", trigger: "21" },
-          { value: 2, label: "No", trigger: "21" },
-        ],
+      delay: 5000,
+      message: "Are you interested to know how much progress we made this year?",
+      trigger: "21",
     },
     {
       id: "21",
-      delay: 4000,
-      message: "A donation of 50 cents will be enough to help us out! It is clear from the personality test results that you really are a positive person. Positive attitudes always are something that we want to share with the world. Together, we can save more lives and provide loving homes for homeless animals.",
-      trigger: "22",
+      options: [
+          { value: 1, label: "Yes", trigger: "22" },
+          { value: 2, label: "No", trigger: "22" },
+        ],
     },
     {
       id: "22",
-      delay: 9000,
-      message: "Last but not least, I'd like to leave you with my favorite quote. Do you know what it is?",
+      delay: 5000,
+      message: "This year, 48% of the 4404 shelters in U.S. have achieved a save rate of 90% or more.",
       trigger: "23",
     },
     {
       id: "23",
-      options: [
-          { value: 1, label: "Yes", trigger: "24" },
-          { value: 2, label: "No", trigger: "24" },
-        ],
+      delay: 5000,
+      message: "This means that 3.5 million cats and dogs have been saved!",
+      trigger: "24",
     },
     {
       id: "24",
-      delay: 4000,
-      message: "My favorite quote is: What we think, we become.",
+      delay: 5000,
+      message: "Interested to find out more about us?",
       trigger: "25",
     },
     {
       id: "25",
+      options: [
+          { value: 1, label: "Yes", trigger: "26" },
+          { value: 2, label: "No", trigger: "26" },
+        ],
+    },
+    {
+      id: "26",
+      delay: 5000,
+      message: "85% of our supporters show with their personality test results that they are an great person with many good qualities just like you.",
+      trigger: "27",
+    },
+    {
+      id: "27",
+      delay: 5000,
+      message: "Therefore, we need your help more than ever to get across the finish line.",
+      trigger: "28",
+    },
+    {
+      id: "28",
+      delay: 5000,
+      message: "Are you interested to find out how you can help us?",
+      trigger: "29",
+    },
+    {
+      id: "29",
+      options: [
+          { value: 1, label: "Yes", trigger: "30" },
+          { value: 2, label: "No", trigger: "30" },
+        ],
+    },
+    {
+      id: "30",
+      delay: 5000,
+      message: "A donation of 50 cents will be enough to help us out!",
+      trigger: "31",
+    },
+    {
+      id: "31",
+      delay: 5000,
+      message: "It is clear from the personality test results that you really are a positive person.",
+      trigger: "32",
+    },
+    {
+      id: "32",
+      delay: 5000,
+      message: "Positive attitudes always are something that we want to share with the world. ",
+      trigger: "33",
+    },
+    {
+      id: "33",
+      delay: 5000,
+      message: "Together, we can save more lives and provide loving homes for homeless animals.",
+      trigger: "34",
+    },
+    {
+      id: "34",
+      delay: 5000,
+      message: "Last but not least, I'd like to leave you with my favorite quote. Do you know what it is?",
+      trigger: "35",
+    },
+    {
+      id: "35",
+      options: [
+          { value: 1, label: "Yes", trigger: "36" },
+          { value: 2, label: "No", trigger: "36" },
+        ],
+    },
+    {
+      id: "36",
+      delay: 5000,
+      message: "My favorite quote is: What we think, we become.",
+      trigger: "37",
+    },
+    {
+      id: "37",
       message: "Thank you! Please close this website and go back to the survey!",
       end:true,
 
     },
+
   ];
 
   return (
